2452dc5276ba2966740b08abf5c1c6f0ce3b1ab2
Jesse Culver <jesseculver92@gmail.com>
2019-04-07 20:40:48 -0500

    Spawn setting entity updated to use network variables

--- gamemodes/storybook/entities/entities/stb_spawn_settings.lua
+++ gamemodes/storybook/entities/entities/stb_spawn_settings.lua
@@ -1,76 +1,138 @@
 --This entity is used by mappers to set some spawn settings
 
+AddCSLuaFile()
+
 ENT.Type = "point"
 ENT.Base = "base_point"
+
+-- KeyValues
+--local enabled = false
+
+function ENT:AcceptInput(name,activator,caller,message)
+  if name == "Apply" then
+    self:ApplySettings()
+  end
+end
 
 function ENT:KeyValue(key, value)
   if key == "startingArmor" and value ~= 0 then
     if tonumber(value) < 0 then
       value = 0
     end
-    GAMEMODE.startingArmor = value
+    self:SetStartingArmor(value)
   elseif key == "respawnArmor" and value ~= 0 then
     if tonumber(value) < 0 then
       value = 0
     end
-    GAMEMODE.respawnArmor = value
+    self:SetRespawnArmor(value)
   elseif key == "startingHealth" and value ~= 100 then
     if tonumber(value) <= 0 then
       value = 1
     end
-    GAMEMODE.startingHealth = value
+    self:SetStartingHealth(value)
   elseif key == "respawnHealth" and value ~= 100 then
     if tonumber(value) <= 0 then
       value = 1
     end
-    GAMEMODE.respawnHealth = value
+    self:SetRespawnHealth(value)
   elseif key == "maxHealth" and value ~= 100 then
     if tonumber(value) <= 0 then
       value = 1
     end
-    GAMEMODE.maxHealth = value
+    self:SetPlyMaxHealth(value)
   elseif key == "solidPlayer" then
     if value == 0 then
-      GAMEMODE.solidPlayer = false
+      self:SetSolidPlayer(false)
     else
-      GAMEMODE.solidPlayer = true
+      self:SetSolidPlayer(true)
     end
   elseif key == "canWalk" then
     if tonumber(value) == 0 then
-      GAMEMODE.canWalk = false
+      self:SetPlyCanWalk(false)
     else
-      GAMEMODE.canWalk = true
+      self:SetPlyCanWalk(true)
     end
   elseif key == "canSprint" then
     if tonumber(value) == 0 then
-      GAMEMODE.canSprint = false
+      self:SetCanSprint(false)
     else
-      GAMEMODE.canSprint = true
+      self:SetCanSprint(true)
     end
   elseif key == "canZoom" then
     if tonumber(value) == 0 then
-      GAMEMODE.canZoom = false
+      self:SetPlyCanZoom(false)
     else
-      GAMEMODE.canZoom = true
+      self:SetPlyCanZoom(true)
     end
   elseif key == "plyModel" and value ~= "" then
     if util.IsValidModel(value) then
       util.PrecacheModel(value)
-      GAMEMODE.playermodel = value
+      self:SetPlyModel(value)
     else
       print("INVALID PLAYER MODEL SET")
     end
   elseif key == "jumpPower" then
-    GAMEMODE.jumpPower = value
+    self:SetPlyJumpPower(value)
   elseif key == "runSpeed" then
-    GAMEMODE.runSpeed = value
+    self:SetPlyRunSpeed(value)
   elseif key == "walkSpeed"  then
-    GAMEMODE.walkSpeed = value
+    self:SetPlyWalkSpeed(value)
   elseif key == "flashlightAllowed" then
     if tonumber(value) == 0 then
-      GAMEMODE.flashlightAllowed = false
+      self:SetFlashlightAllowed(false)
     else
-      GAMEMODE.flashlightAllowed = true
+      self:SetFlashlightAllowed(true)
     end
+  end
+end
+
+function ENT:ApplySettings()
+    GAMEMODE.startingArmor      = self:GetStartingArmor()
+    GAMEMODE.respawnArmor       = self:GetRespawnArmor()
+    GAMEMODE.startingHealth     = self:GetStartingHealth()
+    GAMEMODE.respawnHealth      = self:GetRespawnHealth()
+    GAMEMODE.maxHealth          = self:GetPlyMaxHealth()
+    GAMEMODE.solidPlayer        = self:GetSolidPlayer()
+    GAMEMODE.canWalk            = self:GetPlyCanWalk()
+    GAMEMODE.canSprint          = self:GetCanSprint()
+    GAMEMODE.canZoom            = self:GetPlyCanZoom()
+    GAMEMODE.playermodel        = self:GetPlyModel()
+    GAMEMODE.jumpPower          = self:GetPlyJumpPower()
+    GAMEMODE.runSpeed           = self:GetPlyRunSpeed()
+    GAMEMODE.walkSpeed          = self:GetPlyWalkSpeed()
+    GAMEMODE.flashlightAllowed  = self:GetFlashlightAllowed()
+end
+
+function ENT:SetupDataTables()
+  self:NetworkVar("Int", 0,     "StartingArmor")
+  self:NetworkVar("Int", 1,     "RespawnArmor")
+  self:NetworkVar("Int", 2,     "StartingHealth")
+  self:NetworkVar("Int", 3,     "RespawnHealth")
+  self:NetworkVar("Int", 4,     "PlyMaxHealth")
+  self:NetworkVar("Int", 5,     "PlyJumpPower")
+  self:NetworkVar("Int", 6,     "PlyRunSpeed")
+  self:NetworkVar("Int", 7,     "PlyWalkSpeed")
+  self:NetworkVar("Bool", 0,    "SolidPlayer")
+  self:NetworkVar("Bool", 1,    "PlyCanWalk")
+  self:NetworkVar("Bool", 2,    "CanSprint")
+  self:NetworkVar("Bool", 3,    "PlyCanZoom")
+  self:NetworkVar("Bool", 4,    "FlashlightAllowed")
+  self:NetworkVar("String", 0,  "PlyModel")
+
+  if SERVER then
+    self:SetStartingArmor(0)
+    self:SetRespawnArmor(0)
+    self:SetStartingHealth(100)
+    self:SetRespawnHealth(100)
+    self:SetPlyMaxHealth(100)
+    self:SetPlyJumpPower(160)
+    self:SetPlyRunSpeed(400)
+    self:SetPlyWalkSpeed(200)
+    self:SetSolidPlayer(false)
+    self:SetPlyCanWalk(true)
+    self:SetCanSprint(true)
+    self:SetPlyCanZoom(true)
+    self:SetFlashlightAllowed(true)
+    self:SetPlyModel("models/player/Group03/Male_05.mdl")
   end
 end
