7f18205bb6d3c68b0fa2e69216fe6c42b5655363
Jesse Culver <jesseculver92@gmail.com>
2019-05-08 01:21:47 -0500

    Rework stb_player_color to use keyvalues

--- gamemodes/storybook/entities/entities/stb_player_color.lua
+++ gamemodes/storybook/entities/entities/stb_player_color.lua
@@ -3,20 +3,26 @@
 ENT.Type = "point"
 ENT.Base = "base_point"
 
-function ENT:AcceptInput(name,activator,caller,message)
-  if name == "SetPlayerColor" then
-    newColor = string.ToColor(message)
-    if activator:IsValid() == false then
-      return
-    end
-    if activator:IsPlayer()then
-      activator:SetPlayerColor(Vector(newColor.r/255,newColor.g/255,newColor.b/255))
+function ENT:KeyValue(key, value)
+  if SERVER then
+    if key == "Color" and value ~= "" then
+      self:SetNWVector("plyColor", Vector(value))
     end
   end
-  if name == "SetAllPlayerColors" then
-    newColor = string.ToColor(message)
-    for k, v in pairs(player.GetAll()) do
-      v:SetPlayerColor(Vector(newColor.r/255,newColor.g/255,newColor.b/255))
+end
+
+function ENT:AcceptInput(name,activator,caller,message)
+  if SERVER then
+    if name == "SetPlayerColor" then
+      if activator:IsValid() == false then return end
+      if activator:IsPlayer()then
+        activator:SetPlayerColor(Vector(self:GetNWVector("plyColor", Vector(0,0,0))))
+      end
+    end
+    if name == "SetAllPlayerColors" then
+      for k, v in pairs(player.GetAll()) do
+        v:SetPlayerColor(Vector(self:GetNWVector("plyColor", Vector(0,0,0))))
+      end
     end
   end
 end
