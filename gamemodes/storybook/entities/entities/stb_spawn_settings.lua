cf06860475d623d107cb3d5f5e3cc41e41e43a9d
Jesse Culver <jesseculver92@gmail.com>
2019-05-12 00:18:11 -0500

    Refactored stb_spawn_settings

--- gamemodes/storybook/entities/entities/stb_spawn_settings.lua
+++ gamemodes/storybook/entities/entities/stb_spawn_settings.lua
@@ -1,12 +1,9 @@
---This entity is used by mappers to set some spawn settings
+-- This entity is used by mappers to set some spawn settings
 
 AddCSLuaFile()
 
 ENT.Type = "point"
 ENT.Base = "base_point"
-
--- KeyValues
---local enabled = false
 
 function ENT:AcceptInput(name,activator,caller,message)
   if name == "Apply" then
@@ -19,120 +16,86 @@ function ENT:KeyValue(key, value)
     if tonumber(value) < 0 then
       value = 0
     end
-    self:SetStartingArmor(value)
+    self:SetNWInt("startingArmor",value)
   elseif key == "respawnArmor" and value ~= 0 then
     if tonumber(value) < 0 then
       value = 0
     end
-    self:SetRespawnArmor(value)
+    self:SetNWInt("respawnArmor", value)
   elseif key == "startingHealth" and value ~= 100 then
     if tonumber(value) <= 0 then
       value = 1
     end
-    self:SetStartingHealth(value)
+    self:SetNWInt("startingHealth",value)
   elseif key == "respawnHealth" and value ~= 100 then
     if tonumber(value) <= 0 then
       value = 1
     end
-    self:SetRespawnHealth(value)
+    self:SetNWInt("respawnHealth",value)
   elseif key == "maxHealth" and value ~= 100 then
     if tonumber(value) <= 0 then
       value = 1
     end
-    self:SetPlyMaxHealth(value)
+    self:SetNWInt("maxHealth",value)
   elseif key == "solidPlayer" then
     if value == 0 then
-      self:SetSolidPlayer(false)
+      self:SetNWBool("solidPlayer", false)
     else
-      self:SetSolidPlayer(true)
+      self:SetNWBool("solidPlayer", true)
     end
   elseif key == "canWalk" then
     if tonumber(value) == 0 then
-      self:SetPlyCanWalk(false)
+      self:SetNWBool("canWalk",false)
     else
-      self:SetPlyCanWalk(true)
+      self:SetNWBool("canWalk",true)
     end
   elseif key == "canSprint" then
     if tonumber(value) == 0 then
-      self:SetCanSprint(false)
+      self:SetNWBool("canSprint",false)
     else
-      self:SetCanSprint(true)
+      self:SetNWBool("canSprint",true)
     end
   elseif key == "canZoom" then
     if tonumber(value) == 0 then
-      self:SetPlyCanZoom(false)
+      self:SetNWBool("canZoom",false)
     else
-      self:SetPlyCanZoom(true)
+      self:SetNWBool("canZoom",true)
     end
   elseif key == "plyModel" and value ~= "" then
     if util.IsValidModel(value) then
       util.PrecacheModel(value)
-      self:SetPlyModel(value)
+      self:SetNWString("plyModel", value)
     else
       print("INVALID PLAYER MODEL SET")
     end
   elseif key == "jumpPower" then
-    self:SetPlyJumpPower(value)
+    self:SetNWInt("jumpPower",value)
   elseif key == "runSpeed" then
-    self:SetPlyRunSpeed(value)
+    self:SetNWInt("runSpeed",value)
   elseif key == "walkSpeed"  then
-    self:SetPlyWalkSpeed(value)
+    self:SetNWInt("walkSpeed",value)
   elseif key == "flashlightAllowed" then
     if tonumber(value) == 0 then
-      self:SetFlashlightAllowed(false)
+      self:SetNWBool("flashlightAllowed",false)
     else
-      self:SetFlashlightAllowed(true)
+      self:SetNWBool("flashlightAllowed",true)
     end
   end
 end
 
 function ENT:ApplySettings()
-    GAMEMODE.startingArmor      = self:GetStartingArmor()
-    GAMEMODE.respawnArmor       = self:GetRespawnArmor()
-    GAMEMODE.startingHealth     = self:GetStartingHealth()
-    GAMEMODE.respawnHealth      = self:GetRespawnHealth()
-    GAMEMODE.maxHealth          = self:GetPlyMaxHealth()
-    GAMEMODE.solidPlayer        = self:GetSolidPlayer()
-    GAMEMODE.canWalk            = self:GetPlyCanWalk()
-    GAMEMODE.canSprint          = self:GetCanSprint()
-    GAMEMODE.canZoom            = self:GetPlyCanZoom()
-    GAMEMODE.playermodel        = self:GetPlyModel()
-    GAMEMODE.jumpPower          = self:GetPlyJumpPower()
-    GAMEMODE.runSpeed           = self:GetPlyRunSpeed()
-    GAMEMODE.walkSpeed          = self:GetPlyWalkSpeed()
-    GAMEMODE.flashlightAllowed  = self:GetFlashlightAllowed()
-end
-
-function ENT:SetupDataTables()
-  self:NetworkVar("Int", 0,     "StartingArmor")
-  self:NetworkVar("Int", 1,     "RespawnArmor")
-  self:NetworkVar("Int", 2,     "StartingHealth")
-  self:NetworkVar("Int", 3,     "RespawnHealth")
-  self:NetworkVar("Int", 4,     "PlyMaxHealth")
-  self:NetworkVar("Int", 5,     "PlyJumpPower")
-  self:NetworkVar("Int", 6,     "PlyRunSpeed")
-  self:NetworkVar("Int", 7,     "PlyWalkSpeed")
-  self:NetworkVar("Bool", 0,    "SolidPlayer")
-  self:NetworkVar("Bool", 1,    "PlyCanWalk")
-  self:NetworkVar("Bool", 2,    "CanSprint")
-  self:NetworkVar("Bool", 3,    "PlyCanZoom")
-  self:NetworkVar("Bool", 4,    "FlashlightAllowed")
-  self:NetworkVar("String", 0,  "PlyModel")
-
-  if SERVER then
-    self:SetStartingArmor(0)
-    self:SetRespawnArmor(0)
-    self:SetStartingHealth(100)
-    self:SetRespawnHealth(100)
-    self:SetPlyMaxHealth(100)
-    self:SetPlyJumpPower(160)
-    self:SetPlyRunSpeed(400)
-    self:SetPlyWalkSpeed(200)
-    self:SetSolidPlayer(false)
-    self:SetPlyCanWalk(true)
-    self:SetCanSprint(true)
-    self:SetPlyCanZoom(true)
-    self:SetFlashlightAllowed(true)
-    self:SetPlyModel("models/player/Group03/Male_05.mdl")
-  end
+    GAMEMODE.startingArmor      = self:GetNWInt("startingArmor", 0)
+    GAMEMODE.respawnArmor       = self:GetNWInt("respawnArmor", 0)
+    GAMEMODE.startingHealth     = self:GetNWInt("startingHealth", 100)
+    GAMEMODE.respawnHealth      = self:GetNWInt("respawnHealth", 100)
+    GAMEMODE.maxHealth          = self:GetNWInt("maxHealth", 100)
+    GAMEMODE.solidPlayer        = self:GetNWBool("solidPlayer", false)
+    GAMEMODE.canWalk            = self:GetNWBool("canWalk", true)
+    GAMEMODE.canSprint          = self:GetNWBool("canSprint", true)
+    GAMEMODE.canZoom            = self:GetNWBool("canZoom", true)
+    GAMEMODE.playermodel        = self:GetNWString("plyModel", "models/player/Group03/Male_05.mdl")
+    GAMEMODE.jumpPower          = self:GetNWInt("jumpPower", 160)
+    GAMEMODE.runSpeed           = self:GetNWInt("runSpeed", 400)
+    GAMEMODE.walkSpeed          = self:GetNWInt("walkSpeed", 200)
+    GAMEMODE.flashlightAllowed  = self:GetNWBool("flashlightAllowed", true)
 end
